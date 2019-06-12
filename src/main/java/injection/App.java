ee63e17dd4b1ad325aa87e5f258d3342483c855d
Clement ORMAUX <clement.ormaux@gmail.com>
2019-06-12 15:13:31 +0200

    commit sql insert et select

--- src/main/java/injection/App.java
+++ src/main/java/injection/App.java
@@ -1,11 +1,10 @@
-package intro;
+package injection;
 
 import java.sql.*;
 
-public class AppJava {
+public class App {
 
 	public static void main(String[] args) throws SQLException {
-
 		System.out.println("Hello World !");
 		// pour wamp : useLegacyDatetimeCode=false&serverTimezone=UTC
 		String url = "jdbc:mysql://localhost:3306/book?useSSl=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
@@ -27,12 +26,13 @@ public class AppJava {
 				.executeQuery("SELECT * FROM contact INNER JOIN address ON contact.address_id = address.id");
 
 		while (rsSelect.next()) {
-			System.out.println(rsSelect.getString("email") + " " + rsSelect.getString("first_name")
-					+ " " + rsSelect.getString("last_name") + " " + rsSelect.getString("details"));
+			System.out.println(rsSelect.getString("email") + " " + rsSelect.getString("first_name") + " "
+					+ rsSelect.getString("last_name") + " " + rsSelect.getString("details"));
 		}
 
 		rsSelect.close();
 		st.close();
 		connection.close();
 	}
+
 }
