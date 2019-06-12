294810c370087f761996b94fee84611e8f444aff
Clement ORMAUX <clement.ormaux@gmail.com>
2019-06-12 15:29:57 +0200

    commit connexion

--- src/main/java/injection/App.java
+++ src/main/java/injection/App.java
@@ -1,36 +1,35 @@
 package injection;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class App {
 
+	private static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) throws SQLException {
-		System.out.println("Hello World !");
+		System.out.println("######### Boite de login ##########");
 		// pour wamp : useLegacyDatetimeCode=false&serverTimezone=UTC
 		String url = "jdbc:mysql://localhost:3306/book?useSSl=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
 		String login = "root";
 		String pwd = "";
+
 		Connection connection = DriverManager.getConnection(url, login, pwd);
 		Statement st = connection.createStatement();
-		st.executeUpdate("INSERT INTO address (details) VALUES ('11 rue des Freres Lumieres')",
-				Statement.RETURN_GENERATED_KEYS);
-		ResultSet rs = st.getGeneratedKeys();
+
+		System.out.println("Entrez votre login : ");
+		String loginUser = sc.nextLine();
+		System.out.println("Entrez votre mot de passe : ");
+		String pwdUSer = sc.nextLine();
+
+		ResultSet rs = st.executeQuery(
+				"SELECT * FROM contact WHERE email = '" + loginUser + "' AND first_name = '" + pwdUSer + "' ");
 		if (rs.next()) {
-			st.executeUpdate(
-					"INSERT INTO contact (email, first_name, last_name, address_id) VALUES ('test@gmail.com', 'Cl', 'Or', "
-							+ rs.getLong(1) + ")");
+			System.out.println("Bienvenue dans votre espace de travail : " + rs.getString("first_name") + " " + rs.getString("last_name"));
+		} else {
+			System.out.println("Erreur : login / mot de passe incorrect");
 		}
 		rs.close();
-
-		ResultSet rsSelect = st
-				.executeQuery("SELECT * FROM contact INNER JOIN address ON contact.address_id = address.id");
-
-		while (rsSelect.next()) {
-			System.out.println(rsSelect.getString("email") + " " + rsSelect.getString("first_name") + " "
-					+ rsSelect.getString("last_name") + " " + rsSelect.getString("details"));
-		}
-
-		rsSelect.close();
 		st.close();
 		connection.close();
 	}
