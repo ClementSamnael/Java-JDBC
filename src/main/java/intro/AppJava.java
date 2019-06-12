53291e5ac203e6742790aa8f7226b4f87b1ded28
Clement ORMAUX <clement.ormaux@gmail.com>
2019-06-12 14:30:58 +0200

    premier commit de la bbd

--- src/main/java/intro/AppJava.java
+++ src/main/java/intro/AppJava.java
@@ -7,17 +7,17 @@ public class AppJava {
 	public static void main(String[] args) throws SQLException {
 
 		System.out.println("Hello World !");
+		// pour wamp : useLegacyDatetimeCode=false&serverTimezone=UTC
 		String url = "jdbc:mysql://localhost:3306/book?useSSl=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
 		String login = "root";
 		String pwd = "";
 		Connection connection = DriverManager.getConnection(url, login, pwd);
+		Statement st = connection.createStatement();		
 		
-		try {
-
-		} catch (Exception e) {
-
-		} finally {
-
-		}
+		st.executeUpdate("INSERT INTO contact (email,first_name,last_name,address_id) VALUES ('co@gmail.com', 'Clement', 'Ormaux', 1)");
+		
+		
+		st.close();
+		connection.close();
 	}
 }
